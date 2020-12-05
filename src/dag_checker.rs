83e63fbb8a9546dd21d97489fa5a5436161ae0aa
Gnonpi <legnonpi@gmail.com>
2020-12-05 18:32:19 +0100

    implementing find source and find sinks

--- src/dag_checker.rs
+++ src/dag_checker.rs
@@ -1,8 +1,14 @@
 use crate::type_definition::NodeId;
 use crate::errors::YoshiError;
 use crate::dag::Dag;
+use log::{debug, info};
 use petgraph::algo::{is_cyclic_directed};
+use petgraph::graphmap::NeighborsDirected;
+use petgraph::Direction;    
 
+
+/// Check if there is a cycle in the graph of our Dag
+/// A DAG with a cycle is, by definition, wrong
 fn check_cycle(dag: &Dag) -> Result<(), YoshiError> {
     /*
     if is_cyclic_directed(dag.graph_nodes) {
@@ -20,11 +26,50 @@ fn check_cycle(dag: &Dag) -> Result<(), YoshiError> {
     })
 }
 
+/// Find source nodes in Dag
+/// Source nodes are nodes without incoming edges, without parents
 fn find_source_nodes(dag: &Dag) -> Vec<NodeId> {
-    vec![]
+    if dag.graph_nodes.node_count() == 0 {
+        info!("Dag has no nodes, cannot find sources");
+        return vec![]
+    }
+    if dag.graph_nodes.node_count() == 1 {
+        info!("Dag has only one node, considering it as source");
+        let id_node = dag.graph_nodes.nodes().next().unwrap();
+        return vec![id_node]
+    }
+    let mut result = vec![];
+    for node in dag.graph_nodes.nodes() {
+        let incoming_nodes: Vec<NodeId> = dag.graph_nodes.neighbors_directed(node, Direction::Incoming).collect();
+        if incoming_nodes.len() == 0 {
+            result.push(node);
+        }
+    }
+    result
 }
 
+/// Find sinks nodes in Dag
+/// Sink nodes are nodes without outgoing edges, without children
 fn find_sink_nodes(dag: &Dag) -> Vec<NodeId> {
-    vec![]
+    if dag.graph_nodes.node_count() == 0 {
+        info!("Dag has no nodes, cannot find sinks");
+        return vec![]
+    }
+    if dag.graph_nodes.node_count() == 1 {
+        info!("Dag has only one node, considering it as sink");
+        let id_node = dag.graph_nodes.nodes().next().unwrap();
+        return vec![id_node]
+    }
+    let mut result = vec![];
+    for node in dag.graph_nodes.nodes() {
+        let outgoing_nodes: Vec<NodeId> = dag.graph_nodes.neighbors_directed(node, Direction::Outgoing).collect();
+        if outgoing_nodes.len() == 0 {
+            result.push(node);
+        }
+    }
+    result
 }
 
+#[cfg(test)]
+#[path = "./dag_checker_test.rs"]
+mod dag_checker_test;
--- src/dag_checker_test.rs
+++ src/dag_checker_test.rs
@@ -1,3 +1,9 @@
+use crate::task_definition::DummyTaskDefinition;
+use crate::type_definition::{NodeId, RunnerId};
+use crate::task_node::TaskNode;
+use crate::dag::Dag;
+use crate::dag_checker::{find_source_nodes, find_sink_nodes};
+
 #[test]
 fn it_can_find_one_cycle() {
     assert_eq!(1, 2);
@@ -10,17 +16,48 @@ assert_eq!(1, 2);
 
 #[test]
 fn it_can_find_sources_nodes() {
-assert_eq!(1, 2);
+    let mut dag = Dag::new();
+    let res = find_source_nodes(&dag);
+    assert_eq!(res.len(), 0);
+
+    let t_def = DummyTaskDefinition {};
+    let first_node = TaskNode::new(Box::new(t_def), RunnerId::Fake);
+    let first_id = first_node.id_node.clone();
+    dag.add_task(first_node, None, None);
+    let res = find_source_nodes(&dag);
+    assert_eq!(res, vec![first_id]);
+
+    let t_def = DummyTaskDefinition {};
+    let second_node = TaskNode::new(Box::new(t_def), RunnerId::Fake);
+    dag.add_task(second_node, Some(vec![&first_id]), None);
+    let res = find_source_nodes(&dag);
+    assert_eq!(res, vec![first_id]);
 }
 
 #[test]
 fn it_can_find_no_source_nodes() {
-assert_eq!(1, 2);
+    assert_eq!(1, 2);
 }
 
 #[test]
 fn it_can_find_sink_nodes() {
-assert_eq!(1, 2);
+    let mut dag = Dag::new();
+    let res = find_source_nodes(&dag);
+    assert_eq!(res.len(), 0);
+
+    let t_def = DummyTaskDefinition {};
+    let first_node = TaskNode::new(Box::new(t_def), RunnerId::Fake);
+    let first_id = first_node.id_node.clone();
+    dag.add_task(first_node, None, None);
+    let res = find_sink_nodes(&dag);
+    assert_eq!(res, vec![first_id]);
+
+    let t_def = DummyTaskDefinition {};
+    let second_node = TaskNode::new(Box::new(t_def), RunnerId::Fake);
+    let second_id = second_node.id_node.clone();
+    dag.add_task(second_node, Some(vec![&first_id]), None);
+    let res = find_sink_nodes(&dag);
+    assert_eq!(res, vec![second_id]);
 }
 
 #[test]
