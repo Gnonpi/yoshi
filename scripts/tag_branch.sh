de70fe8f44420349c48ac52ffbe9ff49c663daca
dvivies <dvivies@geoblink.com>
2020-12-13 00:44:38 +0100

    try adding gh_token via travis ui

--- scripts/tag_branch.sh
+++ scripts/tag_branch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -ev
+set -e
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 TOML_VERSION=$(cat ./Cargo.toml | grep version | head -1 | grep -oP '\d{1,2}\.\d{1,2}\.\d{1,2}')
 git tag -a ${TOML_VERSION} -m "${TRAVIS_COMMIT_MESSAGE}"
