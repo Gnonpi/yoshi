b1ff3456239ebaaa42df41c11e89b7a2aeafe9d3
dvivies <dvivies@geoblink.com>
2020-12-12 15:37:23 +0100

    checking if cache is used

--- scripts/tag_branch.sh
+++ scripts/tag_branch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e
+set -ev
 
 TOML_VERSION=$(cat ./Cargo.toml | grep version | head -1 | grep -oP '\d{1,2}\.\d{1,2}\.\d{1,2}')
-echo ${TOML_VERSION}
-echo ${TRAVIS_COMMIT_MESSAGE}
-# git tag -a ${TOML_VERSION} -m ${TRAVIS_COMMIT_MESSAGE}
+git tag -a ${TOML_VERSION} -m ${TRAVIS_COMMIT_MESSAGE}
+git push origin --tags
