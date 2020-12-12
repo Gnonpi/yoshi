728d8fd8a2e4c4bb5f92e704bc1cb295217865f4
dvivies <dvivies@geoblink.com>
2020-12-12 15:54:05 +0100

    adapting my script to run w preinstalled py3

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -5,7 +5,9 @@ If there's already a tag with a version inferior or equal to the one in toml,
 we stop
 
 The goal is not to have a brillant Python demonstration,
-that's why I'm using prints and unittest and keeping it simple
+that's why I'm using prints and unittest and keeping it simple.
+TravisCI is running 3.5.2, I usually go with 3.6+ 
+but configuring pyenv started to eat me too much time.
 '''
 import re
 from pathlib import Path
@@ -30,7 +32,7 @@ class SemanticVersion:
     def from_string(cls, semver_string: str) -> 'SemanticVersion':
         match = RE_SEMVER.match(semver_string)
         if match is None:
-            raise ValueError(f"Could not parse semantic version from '{semver_string}'")
+            raise ValueError("Could not parse semantic version from '{}'".format(semver_string))
         matches = match.groupdict()
         return cls(
             int(matches['major']), 
@@ -52,7 +54,9 @@ class SemanticVersion:
         return self == other or self < other
         
     def __repr__(self):
-        return f"SemanticVersion(major={self.major}, minor={self.minor}, patch={self.patch})"
+        return "SemanticVersion(major={}, minor={}, patch={})".format(
+            self.major, self.minor, self.patch
+        )
 
 
 def read_git_tags() -> List[SemanticVersion]:
@@ -69,31 +73,31 @@ def read_cargo_toml() -> SemanticVersion:
     """
     Read version written in current Cargo.toml
     """
-    print(f'Reading Cargo.toml at {PATH_CARGO_TOML}')
-    with open(PATH_CARGO_TOML, 'r') as f:
+    print('Reading Cargo.toml at {}'.format(PATH_CARGO_TOML))
+    with open(str(PATH_CARGO_TOML), 'r') as f:
         # if the crate version is not the first version key, this is wrong
         for line in f:
             if line.startswith('version'):
                 sem_ver = line.split('=')[1].strip().strip('\"')
                 return SemanticVersion.from_string(sem_ver)
-    raise KeyError(f'Found no semantic version in Cargo.toml')
+    raise KeyError('Found no semantic version in Cargo.toml')
 
 def main():
     git_tag_versions = read_git_tags()
-    print(f'Got {len(git_tag_versions)} from git tags')
+    print('Got {} from git tags'.format(len(git_tag_versions)))
     
     toml_version = read_cargo_toml()
-    print(f'Got {toml_version} from Cargo.toml')
+    print('Got {} from Cargo.toml'.format(toml_version))
     
     if len(git_tag_versions) == 0:
         print('Since we got no git tags, version should be ok')
     
     if toml_version in git_tag_versions:
-        raise RuntimeError(f"Version in toml '{toml_version}' already found in git tags")
+        raise RuntimeError("Version in toml '{}' already found in git tags".format(toml_version))
     
     max_git_tag_version = max(git_tag_versions)
     if toml_version <= max_git_tag_version:
-        raise RuntimeError(f"Version in toml '{toml_version}' lower or equal to max git tag '{max_git_tag_version}'")
+        raise RuntimeError("Version in toml '{}' lower or equal to max git tag '{}'".format(toml_version, max_git_tag_version))
     print('Everything seems fine')
 
 
