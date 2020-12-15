c45d72b588f985a0ed0b8a0a05047db3e1025e61
dvivies <dvivies@geoblink.com>
2020-12-15 01:08:42 +0100

    adding default source

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,8 @@
 use crate::type_definition::NodeId;
 use crate::task_definition::{TaskDefinitionType, DefinitionArgumentType};
 use crossbeam_channel::TryRecvError;
+use std::error::Error;
+use std::fmt;
 
 /// Basic error for the crate
 #[derive(Debug)]
@@ -55,3 +57,21 @@ pub enum YoshiError {
     // dev
     ToImplementSoon
 }
+
+impl fmt::Display for YoshiError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // todo: implement true error messages for YoshiError
+        // let's use Debug for now
+        write!(f, "Error: {:?}", self)
+    }
+}
+
+impl Error for YoshiError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        // todo: split into subcategories
+        // We don't have sub-errors and implementing all sources is a bit complicating with so much
+        // we'll do it when splitting YoshiError
+        None
+    }
+}
+
